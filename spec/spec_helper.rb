45911f104f5a797248bdff2c8e92e439e35aad50
Sweetlie <libellule.se@gmail.com>
2016-12-31 13:03:23 +0100

    Change VCR matcher to ignore apikey param

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,11 +7,17 @@ require 'dotenv'
 Dotenv.load
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'spec/cassettes'
-  c.hook_into :faraday
-  c.configure_rspec_metadata!
-  c.filter_sensitive_data('BNET_API_KEY') { ENV["BNET_API_KEY"] }
-  c.default_cassette_options = { :record => :once  }
+	c.cassette_library_dir = 'spec/cassettes'
+	c.hook_into :faraday
+	c.configure_rspec_metadata!
+	c.filter_sensitive_data('BNET_API_KEY') { ENV["BNET_API_KEY"] }
+	c.default_cassette_options = {
+		:record => :once,
+		:match_requests_on => [
+		  	:host,
+		  	:method,
+		  	VCR.request_matchers.uri_without_param(:apikey)
+  		] }	
 
 end
 
