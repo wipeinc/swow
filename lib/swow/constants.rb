0c3c3a3043b409ad3a5229e6541d3aede122c364
Sweetlie <libellule.se@gmail.com>
2017-01-06 16:25:29 +0100

    Fix achievements constant

--- lib/swow/constants.rb
+++ lib/swow/constants.rb
@@ -3,7 +3,7 @@
 module Swow
 	module Constants
 		REGIONS = %w(us eu kr tw)
-		CHARACTER_FIELDS = %w(acheivements appearance feed guild hunterPets items
+		CHARACTER_FIELDS = %w(achievements appearance feed guild hunterPets items
 												  mounts pets petSlots professions pvp quests reputation
 												  statistics stats talents titles audit)
 	end
